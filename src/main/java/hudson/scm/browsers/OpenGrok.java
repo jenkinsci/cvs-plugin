58c0253cf28708abb8feb0f76efca6733e8435a2
Michael Clarke <michael.m.clarke@gmail.com>
2013-03-23 20:33:09 +0000

    Removing unused import

--- src/main/java/hudson/scm/browsers/OpenGrok.java
+++ src/main/java/hudson/scm/browsers/OpenGrok.java
@@ -34,7 +34,6 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.export.Exported;
 
-import javax.servlet.ServletException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
