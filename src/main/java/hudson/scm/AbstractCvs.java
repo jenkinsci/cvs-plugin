jenkinsci/cvs-plugin

commit 52c36424d358349f3976b1aeddc2dd74e48f486d

Prevent case-sensitive host name comparison since DNS is case insensitive

src/main/java/hudson/scm/AbstractCvs.java
@@ -315,11 +315,12 @@ public abstract class AbstractCvs extends SCM implements ICvs {
             cvsRoot.setPassword(Secret.toString(repository.getPassword()));
         }
         else {
-            String partialRoot = cvsRoot.getHostName() + ":" + ConnectionFactory.getConnection(cvsRoot).getPort() + cvsRoot.getRepository();
+            String partialRoot = cvsRoot.getHostName().toLowerCase() + ":" + ConnectionFactory.getConnection
+                    (cvsRoot).getPort() + cvsRoot.getRepository();
             String sanitisedRoot = ":" + cvsRoot.getMethod() + ":" + partialRoot;
             for (CvsAuthentication authentication : getDescriptor().getAuthentication()) {
                 CVSRoot authenticationRoot = CVSRoot.parse(authentication.getCvsRoot());
-                String partialAuthenticationRoot = authenticationRoot.getHostName() + ":"
+                String partialAuthenticationRoot = authenticationRoot.getHostName().toLowerCase() + ":"
                         + ConnectionFactory.getConnection(authenticationRoot).getPort() + authenticationRoot.getRepository();
                 String sanitisedAuthenticationRoot = ":" + cvsRoot.getMethod() + ":" + partialAuthenticationRoot;
                 if (sanitisedAuthenticationRoot.equals(sanitisedRoot)

commit c62f6d51ff664c0d703ed9136adc9fa85162e9b9

52c3642 broke use of the plugin for local repositories (CVSROOT=/path/to/repo).

src/main/java/hudson/scm/AbstractCvs.java
@@ -315,7 +315,8 @@ public abstract class AbstractCvs extends SCM implements ICvs {
             cvsRoot.setPassword(Secret.toString(repository.getPassword()));
         }
         else {
-            String partialRoot = cvsRoot.getHostName().toLowerCase() + ":" + ConnectionFactory.getConnection
+            String hostName = cvsRoot.getHostName();
+            String partialRoot = (hostName != null ? hostName.toLowerCase() : "") + ":" + ConnectionFactory.getConnection
                     (cvsRoot).getPort() + cvsRoot.getRepository();
             String sanitisedRoot = ":" + cvsRoot.getMethod() + ":" + partialRoot;
             for (CvsAuthentication authentication : getDescriptor().getAuthentication()) {