817d7692607b95837e2f32ddc61aa64f8cc634d5
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-21 17:16:57 -0400

    extract JS in tagform.jelly

---
+++ src/main/resources/hudson/scm/cvstagging/CvsTagAction/create-another-tag.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('create-another-tag-button')
+        .addEventListener('click', () => {
+            document.getElementById('tagForm').show();
+            document.getElementById('tagButton').style.display = 'none';
+        });
+});
