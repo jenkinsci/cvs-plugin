8de118c69784421da76c022ac7f68c7b41b643d7
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-22 12:14:17 -0400

    address comments

--- src/main/resources/hudson/scm/cvstagging/LegacyTagAction/create-another-tag.js
+++ src/main/resources/hudson/scm/cvstagging/LegacyTagAction/create-another-tag.js
@@ -1,4 +1,4 @@
-ocument.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('create-another-tag-button')
         .addEventListener('click', () => {
