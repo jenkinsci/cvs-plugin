c403207f52cf4be27b554742fa3966ae9d79f273
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-05-06 10:46:18 +0200

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/hudson/scm/LegacyConvertorTest.java
+++ src/test/java/hudson/scm/LegacyConvertorTest.java
@@ -1,19 +1,24 @@
 package hudson.scm;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+
 import java.io.File;
 import java.net.URISyntaxException;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.jvnet.hudson.test.HudsonTestCase;
-import org.jvnet.hudson.test.JenkinsRule;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.assertEquals;
+@WithJenkins
+class LegacyConvertorTest {
 
-public class LegacyConvertorTest {
+    private JenkinsRule jenkinsRule;
 
-    @Rule
-    public JenkinsRule jenkinsRule = new JenkinsRule();
+    @BeforeEach
+    void setUp(JenkinsRule rule) {
+        jenkinsRule = rule;
+    }
 
     // borrowed from core/test/.../TestResultTest, this is probably too complicated
     private String getDataFile(String name) throws URISyntaxException {
@@ -22,9 +27,9 @@ public class LegacyConvertorTest {
 
     // check JENKINS-12582 CVS-Plugin: Password file "${user.home}/.cvspass" is ignored
     @Test
-    public final void testGetPassword() throws URISyntaxException {
+    final void testGetPassword() throws URISyntaxException {
         // FIXME: not sure if setting user.home is safe in all cases?
-        String home=System.getProperty("user.home");
+        String home = System.getProperty("user.home");
         System.setProperty("user.home", getDataFile(""));
         LegacyConvertor instance = LegacyConvertor.getInstance();
         assertEquals("password", instance.getPassword(":pserver:user@example.com/usr/local/cvsroot"));
--- src/test/java/hudson/scm/browsers/CvsFacadeRepositoryBrowserTest.java
+++ src/test/java/hudson/scm/browsers/CvsFacadeRepositoryBrowserTest.java
@@ -7,38 +7,35 @@ import hudson.scm.CvsModule;
 import hudson.scm.CvsRepository;
 import hudson.scm.CvsRepositoryItem;
 import hudson.scm.CvsRepositoryLocation;
-import hudson.scm.ExcludedRegion;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
-import org.jvnet.hudson.test.JenkinsRule;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
-public class CvsFacadeRepositoryBrowserTest {
+@WithJenkins
+class CvsFacadeRepositoryBrowserTest {
 
-    @Rule
-    public JenkinsRule jenkinsRule = new JenkinsRule();
+    private JenkinsRule jenkinsRule;
 
     private CvsFacadeRepositoryBrowser testCase;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp(JenkinsRule rule) {
+        jenkinsRule = rule;
         testCase = new CvsFacadeRepositoryBrowser();
     }
 
     @Test
-    public void testResolveBrowser() throws MalformedURLException {
+    void testResolveBrowser() throws Exception {
         ViewCVS browser = new ViewCVS(new URL("http://localhost/viewcvs/viewcvs.cgi?cvsroot=repo"));
-        CvsRepository repository = new CvsRepository(":pserver:host:port/path/to/repo", false, null, new ArrayList<CvsRepositoryItem>(), new ArrayList<ExcludedRegion>(), -1, browser);
+        CvsRepository repository = new CvsRepository(":pserver:host:port/path/to/repo", false, null, new ArrayList<>(), new ArrayList<>(), -1, browser);
 
         CVSChangeLogSet.CVSChangeLog changelog = new CVSChangeLogSet.CVSChangeLog();
         changelog.setRepository(repository);
@@ -46,20 +43,20 @@ public class CvsFacadeRepositoryBrowserTest {
     }
 
     @Test
-     public void testResolveOldFile() throws MalformedURLException {
+    void testResolveOldFile() {
         CVSChangeLogSet.CVSChangeLog changelog = new CVSChangeLogSet.CVSChangeLog();
         assertNull(testCase.resolveRepositoryBrowser(changelog));
     }
 
     @Test
-    public void testResolveParameterisedRepository() throws IOException {
+    void testResolveParameterisedRepository() throws Exception {
         ViewCVS browser = new ViewCVS(new URL("http://localhost/viewcvs/viewcvs.cgi?cvsroot=repo"));
         FreeStyleProject p = jenkinsRule.createFreeStyleProject();
         p = jenkinsRule.createFreeStyleProject("useful project");
-        p.setScm(new CVSSCM(Arrays.asList(new CvsRepository(":pserver:user@host:${otherPort}/path/to/repo", false, null, new ArrayList<CvsRepositoryItem>(), new ArrayList<ExcludedRegion>(), -1, browser)), false, false, false, false, false, false, false));
+        p.setScm(new CVSSCM(List.of(new CvsRepository(":pserver:user@host:${otherPort}/path/to/repo", false, null, new ArrayList<>(), new ArrayList<>(), -1, browser)), false, false, false, false, false, false, false));
 
 
-        CvsRepository repository = new CvsRepository(":pserver:host:${port}/path/to/repo", false, null, new ArrayList<CvsRepositoryItem>(), new ArrayList<ExcludedRegion>(), -1, null);
+        CvsRepository repository = new CvsRepository(":pserver:host:${port}/path/to/repo", false, null, new ArrayList<>(), new ArrayList<>(), -1, null);
 
 
         CVSChangeLogSet.CVSChangeLog changelog = new CVSChangeLogSet.CVSChangeLog();
@@ -70,18 +67,18 @@ public class CvsFacadeRepositoryBrowserTest {
     }
 
     @Test
-    public void testResolveBrowserFromAlternativeJob() throws IOException {
+    void testResolveBrowserFromAlternativeJob() throws Exception {
         // create a few projects with browsers
         for (int k = 0; k < 10; k++) {
             FreeStyleProject p = jenkinsRule.createFreeStyleProject("job " + k);
-            List<CvsRepository> repositoryList = new ArrayList<CvsRepository>();
+            List<CvsRepository> repositoryList = new ArrayList<>();
             for (int j = 0; j < 5; j++) {
                 CvsModule[] modules = new CvsModule[10];
                 for (int i = 0; i < modules.length; i++) {
                     modules[i] = new CvsModule("remote" + i, "remote" + i);
                 }
                 CvsRepositoryItem item = new CvsRepositoryItem(new CvsRepositoryLocation.HeadRepositoryLocation(), modules);
-                repositoryList.add(new CvsRepository(":pserver:user@host:" + j + "/path/to/repo", false, null, Arrays.asList(item), new ArrayList<ExcludedRegion>(), -1, new ViewCVS(new URL("http://host:" + j + "/viewcvs/viewcvs.cgi?cvsroot=root"))));
+                repositoryList.add(new CvsRepository(":pserver:user@host:" + j + "/path/to/repo", false, null, List.of(item), new ArrayList<>(), -1, new ViewCVS(new URL("http://host:" + j + "/viewcvs/viewcvs.cgi?cvsroot=root"))));
             }
             p.setScm(new CVSSCM(repositoryList, false, false, false, false, false, false, false));
         }
@@ -89,24 +86,24 @@ public class CvsFacadeRepositoryBrowserTest {
         FreeStyleProject p = jenkinsRule.createFreeStyleProject("null browser job");
 
         // now add a job with no browser
-        List<CvsRepository> repositoryList = new ArrayList<CvsRepository>();
+        List<CvsRepository> repositoryList = new ArrayList<>();
         for (int j = 0; j < 5; j++) {
             CvsModule[] modules = new CvsModule[10];
             for (int i = 0; i < modules.length; i++) {
                 modules[i] = new CvsModule("remote" + i, "remote" + i);
             }
             CvsRepositoryItem item = new CvsRepositoryItem(new CvsRepositoryLocation.HeadRepositoryLocation(), modules);
-            repositoryList.add(new CvsRepository(":pserver:user@host:" + j + "/path/to/repo", false, null, Arrays.asList(item), new ArrayList<ExcludedRegion>(), -1, null));
+            repositoryList.add(new CvsRepository(":pserver:user@host:" + j + "/path/to/repo", false, null, List.of(item), new ArrayList<>(), -1, null));
         }
         p.setScm(new CVSSCM(repositoryList, false, false, false, false, false, false, false));
 
         // add a job with the browser we expect to get, and a cvs root that matches our test root
         ViewCVS browser = new ViewCVS(new URL("http://localhost/viewcvs/viewcvs.cgi?cvsroot=repo"));
         p = jenkinsRule.createFreeStyleProject("useful project");
-        p.setScm(new CVSSCM(Arrays.asList(new CvsRepository(":pserver:host:10/path/to/repo", false, null, new ArrayList<CvsRepositoryItem>(), new ArrayList<ExcludedRegion>(), -1, browser)), false, false, false, false, false, false, false));
+        p.setScm(new CVSSCM(List.of(new CvsRepository(":pserver:host:10/path/to/repo", false, null, new ArrayList<>(), new ArrayList<>(), -1, browser)), false, false, false, false, false, false, false));
 
 
-        CvsRepository repository = new CvsRepository(":pserver:host:10/path/to/repo", false, null, new ArrayList<CvsRepositoryItem>(), new ArrayList<ExcludedRegion>(), -1, null);
+        CvsRepository repository = new CvsRepository(":pserver:host:10/path/to/repo", false, null, new ArrayList<>(), new ArrayList<>(), -1, null);
 
         CVSChangeLogSet.CVSChangeLog changelog = new CVSChangeLogSet.CVSChangeLog();
         changelog.setRepository(repository);
@@ -115,7 +112,5 @@ public class CvsFacadeRepositoryBrowserTest {
         //cal again to check caching hasn't broken anything
         assertSame(browser, testCase.resolveRepositoryBrowser(changelog));
     }
-
-
 
 }
